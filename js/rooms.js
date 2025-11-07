840c6e1d8c3cef7c69260c2598f78117e6f9318b
minhsonpromax-spec <minhsonpromax@gmail.com>
2025-11-07 14:06:11 +0700

    update ttl popup

--- js/rooms.js
+++ js/rooms.js
@@ -56,7 +56,7 @@ document.addEventListener("DOMContentLoaded", () => {
     // Tự ẩn sau 5 giây
     setTimeout(() => {
       popup.classList.add("hidden");
-    }, 5000);
+    }, 10000);
   }
   document.getElementById("logPopup").addEventListener("click", async (e) => {
     const popup = e.currentTarget;
